5286e572228a08a2c81c4858e0dfaa5bb347f9e0
Peng Chen <peng.ch@outlook.com>
2018-07-23 22:38:31 +0800

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {  } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,6 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  logs = [{time:new Date(), title:'testing 1'},
+  {time:new Date(), title:'testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2'},
+  {time:new Date(), title:'testing 3 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2'},
+  {time:new Date(), title:'testing 4 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2'},
+  {time:new Date(), title:'testing 5 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2'},
+  {time:new Date(), title:'testing 6 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2 testing 2'} ];
   constructor(public navCtrl: NavController) {
 
   }
